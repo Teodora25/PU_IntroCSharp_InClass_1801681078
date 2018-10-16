265d3cd2cdb0e50b76b0375692f0b51e46e49013
viki2727 <viki2727@abv.bg>
2018-10-16 18:34:34 +0300

    My new music player

--- PUInClassW3/ConsoleApp1/Program.cs
+++ PUInClassW3/ConsoleApp1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Do you nwant to play some music?");
+            String answer = Console.ReadLine();
+            
+            if(answer=="Yes")
+            {
+                Console.WriteLine("Playing music...!!!");
+
+            }
+            else if(answer=="No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("wrong input!");
+            }
+
+
         }
     }
 }
